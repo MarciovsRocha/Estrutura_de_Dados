0922ae2a73f81ba44ffdf4bba02079deca306e9d
Marcio Vinicius de Souza da Rocha <marcio@localhost.localdomain>
2021-03-16 20:51:12 -0300

    Criada pasta para o conteudo da Aula 3

---
+++ Code/src/Aula3/VetoresMatrizes/ApplicationTeste.java
@@ -0,0 +1,12 @@
+package Aula3.VetoresMatrizes;
+
+public class ApplicationTeste {
+    public static void main(String[] args){
+        int[] vetor = new int[5];
+        vetor[0] = 3;
+        vetor[1] = 7;
+        vetor[2] = 11;
+        vetor[3] = 9;
+        vetor[4] = 6;
+    }
+}
